dc10bdaad42f1636db1582e0b544b63f4f72bed7
Nelson Martinez <nelson.martinez@intelity.com>
2022-04-19 01:44:52 -0500

    Test Application first commit

---
+++ TestApplicationPayEvo/API/TestAPIController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TestApplicationPayEvo.Model;
+
+namespace TestApplicationPayEvo
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestAPIController : ControllerBase
+    {
+        private HttpClient httpClient = new HttpClient();
+        private ModelTest1 modelTest;
+        
+
+        public async Task<DataTable> GetTimeTaskAsync()
+        {
+            DataTable dataTable = new DataTable();
+            string path = "https://api.harvestapp.com/v2/time_entries";
+            string result = "";
+            string key = "Bearer 3033552.pt.14cZwk70CfW_4W9LkQNUrOrqoX5VoCPT8kz1NCtzAEfa9Yxd6fIzU_YRtcvt8rIJkGgKY5AH0tjnarIdqb9rtQ";
+            modelTest = null;
+            httpClient.DefaultRequestHeaders.Add("Harvest-Account-ID", "1596805");
+            httpClient.DefaultRequestHeaders.Add("Authorization", key);
+            httpClient.DefaultRequestHeaders.Add("User-Agent", "Harvest API Example");
+            HttpResponseMessage response = await httpClient.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                result = response.Content.ReadAsStringAsync().Result;
+                dataTable = JsonConvert.DeserializeObject<DataTable>(result);
+            }
+            //return product;
+            return dataTable;
+        }
+    }
+}
---
+++ TestApplicationPayEvo/Model/ModelTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestApplicationPayEvo.Model
+{
+    public class ModelTest1
+    {
+        public string id { get; set; }
+        public string spent_date { get; set; }
+        public string user_name { get; set; }
+        public string user_assignment_name { get; set; }
+        public string client_name { get; set; }
+        public string project_name { get; set; }
+        public string task_name { get; set; }
+        public string task_assignment_name { get; set; }
+        public string external_reference { get; set; }
+
+        public string invoice_id { get; set; }
+
+        public decimal hours { get; set; }
+
+
+
+        public string notes { get; set; }
+        public string started_time { get; set; }
+        public string ended_time { get; set; }
+        public string created { get; set; }
+        public string updated { get; set; }
+
+       
+    }
+}
